28bdda62489ff2d25a5bee5423854b08e8ab5517
Allen Galler <allengaller@gmail.com>
2012-06-26 13:18:38 +0800

    Add runner function, which could clean out hacker News official links(You don't need to read them everyday).

--- hr_core.py
+++ hr_core.py
@@ -1,36 +1,47 @@
-#This is the core function in Python.
+# Allen Galler
+# allengaller@gmail.com
 
-import urllib2, webbrowser, os
+import urllib2, webbrowser, os, re
 from HTMLParser import HTMLParser
 
-hacker_url = "http://news.ycombinator.com/"
+hacker_news_url = "http://news.ycombinator.com/"
 
-request = urllib2.Request(hacker_url)
+request = urllib2.Request(hacker_news_url)
 response = urllib2.urlopen(request)
 buffer = response.read()
 
+# Parse links from the mainpage.
 class Parser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.links = []
- 
+
     def handle_starttag(self, tag, attrs):
         #print "Encountered the beginning of a %s tag" % tag
         if tag == "a":
             if len(attrs) == 0: pass
             else:
-                for (variable, value)  in attrs:
-                    if variable == "href":
-                        self.links.append(value)
+                for (variable, value) in attrs:
+                    if variable == "href": self.links.append(value)
 
+        return self.links
+
+# Clean up link list and run.
+def Runner(raw_url_list):
+    clean_url_list = []
+    
+    for raw_url in raw_url_list:
+        if raw_url.find("http") ==0:
+            if raw_url.find("http://ycombinator.com") != 0:
+                print raw_url
+                webbrowser.open(raw_url)
+    return clean_url_list       
+                   
 if __name__ == "__main__":
     hacker_parser = Parser()
     hacker_parser.feed(buffer)
 
-    for items in hacker_parser.links:
-        if items.find("http") == 0:
-            print items
-            webbrowser.open(items)
-
-    hacker_parser.close()
+    clean_list = Runner(hacker_parser.links)
     
+    hacker_parser.close()
+
